ab55a342f225625205882a92d3a07afc1c85cebf
Anais Biscaras <72392239+Loulaty@users.noreply.github.com>
2021-03-15 15:06:08 +0100

    ye

--- platformer-main/src/scenes/Tiled.js
+++ platformer-main/src/scenes/Tiled.js
@@ -2,35 +2,26 @@ class Tiled extends Tableau{
 
     preload() {
         super.preload();
-        // ------pour TILED-------------
-        // important a charger
         this.load.image('tiles', 'assets/tilemaps/tableauTiledTileset.png');
-        //les données du tableau qu'on a créé dans TILED
         this.load.tilemapTiledJSON('map', 'assets/tilemaps/tiles.json');
     }
 
     create() {
         super.create();
 
-        //on en aura besoin...
-        let ici=this;
-
-        //--------chargement de la tile map & configuration de la scène-----------------------
-
-        //important correspondaces 
         this.map = this.make.tilemap({ key: 'map' });
-        //nos images qui vont avec la map
-        this.tileset = this.map.addTilesetImage('tableauTiledTileset', 'tiles');
+        this.tileset = this.map.addTilesetImage('tuiles', 'tiles');
 
 
 
-        this.solides = this.map.createLayer('tuiles', this.tileset, 0, 0);
 
+        this.tuiles = this.map.createLayer('tuiles', this.tileset, 0, 0);
 
-        //on définit les collisions, plusieurs méthodes existent:
+        this.tuiles.setDepth(100000);
 
-        this.solides.setCollisionByProperty({ collides: true });
-        this.lave.setCollisionByProperty({ collides: true });
+        this.tuiles.setCollisionByExclusion(-1,true);
+        this.physics.add.collider(this.player, this.tuiles);
+        
     }
 
 }

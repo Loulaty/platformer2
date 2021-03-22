2b1d47ffe937e7631d6e9c56c8d8fefa885e31fa
Anais Biscaras <72392239+Loulaty@users.noreply.github.com>
2021-03-22 14:38:18 +0100

    Update Tiled.js

--- platformer-main/src/scenes/Tiled.js
+++ platformer-main/src/scenes/Tiled.js
@@ -18,8 +18,8 @@ class Tiled extends Tableau{
 
         this.tuiles.setDepth(100000);
 
-       // this.tuiles.setCollisionByExclusion(-1,true);
-        this.barrieres.setCollisionByProperty({ collide: true});
+        this.tuiles.setCollisionByExclusion(-1,true);
+        //this.barrieres.setCollisionByProperty({ collide: true});
         this.physics.add.collider(this.player, this.tuiles);
 
         let largeurDuTableau=this.map.widthInPixels;

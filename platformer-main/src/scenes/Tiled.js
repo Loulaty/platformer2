5867a16801e65f53d34059790922db6ee37c1f56
Anais Biscaras <72392239+Loulaty@users.noreply.github.com>
2021-05-12 08:37:40 +0200

    commit

--- platformer-main/src/scenes/Tiled.js
+++ platformer-main/src/scenes/Tiled.js
@@ -5,6 +5,18 @@ class Tiled extends Tableau{
         this.load.image('tiles', 'assets/tilemaps/tableauTiledTileset.png');
         this.load.tilemapTiledJSON('map', 'assets/tilemaps/tiles.json');
         this.load.image('star', 'assets/star.png');
+        this.load.image('star2', 'assets/star2.png');
+        this.load.image('monster-violet', 'assets/monster-violet.png');
+        this.load.image('monstre2', 'assets/monstre2.png');
+        this.load.image('monstre3', 'assets/monstre3.png');
+        this.load.image('monstre4', 'assets/monstre4.png');
+        this.load.image('monstre5', 'assets/monstre5.png');
+        this.load.image('monster-fly', 'assets/monster-fly.png');
+        this.load.image('ground', 'assets/platform.png');
+        this.load.image('jump', 'assets/platformJump.png');
+        this.load.image('sky-2', 'assets/sky-2.png');
+        this.load.image('sky', 'assets/sky.png');
+
     }
 
     create() {
@@ -32,6 +44,27 @@ class Tiled extends Tableau{
         this.cameras.main.startFollow(this.player, true, 1, 1);
 
 
+        this.sky=this.add.tileSprite(
+            0,
+            0,
+            this.sys.canvas.width,
+            this.sys.canvas.height,
+            'sky'
+        );
+        this.sky2=this.add.tileSprite(
+            0,
+            0,
+            this.sys.canvas.width,
+            this.sys.canvas.height,
+            'sky-2'
+        );
+        this.sky.setOrigin(0,0);
+        this.sky2.setOrigin(0,0);
+        this.sky.setScrollFactor(0);//fait en sorte que le ciel ne suive pas la caméra
+        this.sky2.setScrollFactor(0);//fait en sorte que le ciel ne suive pas la caméra
+        this.sky2.blendMode=Phaser.BlendModes.ADD;
+
+
           //----------collisions---------------------
 
         //quoi collide avec quoi?
@@ -50,6 +83,44 @@ class Tiled extends Tableau{
         this.physics.add.overlap(this.star, this.platforms);
         this.physics.add.overlap(this.player, this.star, this.ramasserEtoile, null, this);
 
+        let z=1000; //niveau Z qui a chaque fois est décrémenté.
+        this.player.setDepth(z--);
+        this.sky2.setDepth(z--);
+        this.sky.setDepth(z--);
+
+        
+
+    }
+
+    optimizeDisplay(){
+        //return;
+        let world=this.cameras.main.worldView; // le rectagle de la caméra, (les coordonnées de la zone visible)
+
+        // ici vous pouvez appliquer le même principe pour des monstres, des étoiles etc...
+    }
+
+    moveParallax(){
+        //le ciel se déplace moins vite que la caméra pour donner un effet paralax
+        this.sky2.tilePositionX=this.cameras.main.scrollX*0.6;
+        this.sky2.tilePositionY=this.cameras.main.scrollY*0.6;
+        this.sky.tilePositionX=this.cameras.main.scrollX*0.7+100;
+        this.sky.tilePositionY=this.cameras.main.scrollY*0.7+100;
+    }
+
+    update(){
+        super.update();
+        this.moveParallax();
+
+        //optimisation
+        //teste si la caméra a bougé
+        let actualPosition=JSON.stringify(this.cameras.main.worldView);
+        if(
+            !this.previousPosition
+            || this.previousPosition !== actualPosition
+        ){
+            this.previousPosition=actualPosition;
+            this.optimizeDisplay();
+        }
     }
 
 }

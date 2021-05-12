ccea0ab9e70012875d1c390b05b352f00247845f
Anais Biscaras <72392239+Loulaty@users.noreply.github.com>
2021-05-12 08:47:19 +0200

    test

--- platformer-main/assets/tilemaps/tableauTiledTileset.tsx
+++ platformer-main/assets/tilemaps/tableauTiledTileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="tableauTiledTileset" tilewidth="64" tileheight="64" tilecount="98" columns="14">
- <grid orientation="orthogonal" width="32" height="32"/>
+<tileset version="1.4" tiledversion="1.4.3" name="tableauTiledTileset" tilewidth="32" tileheight="32" tilecount="392" columns="28">
+ <grid orientation="orthogonal" width="64" height="64"/>
  <image source="tableauTiledTileset.png" width="896" height="448"/>
 </tileset>

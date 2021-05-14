6d6adb0c60f4755f7cecae3964b57f362c669722
Anais Biscaras <72392239+Loulaty@users.noreply.github.com>
2021-05-14 20:06:46 +0200

    playeranim

--- platformer-main/src/objects/Player.js
+++ platformer-main/src/objects/Player.js
@@ -15,21 +15,21 @@ class Player extends Phaser.Physics.Arcade.Sprite{
 
         this.anims.create({
             key: 'left',
-            frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+            frames: this.anims.generateFrameNumbers('player', { start: 0, end: 7 }),
             frameRate: 10,
             repeat: -1
         });
 
         this.anims.create({
             key: 'right',
-            frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
+            frames: this.anims.generateFrameNumbers('player', { start: 8, end: 15 }),
             frameRate: 10,
             repeat: -1
         });
         this.anims.create({
             key: 'turn',
-            frames: [ { key: 'player', frame: 4 } ],
-            frameRate: 20
+            frames: this.anims.generateFrameNumbers('player', { start: 16, end: 21 }),
+            frameRate: 10
         });
 
 

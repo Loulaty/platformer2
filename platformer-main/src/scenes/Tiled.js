82ef6338f81966746756ed8104e6d7a35aeb62e0
Anais Biscaras <72392239+Loulaty@users.noreply.github.com>
2021-06-02 14:20:50 +0200

    Update Tiled.js

--- platformer-main/src/scenes/Tiled.js
+++ platformer-main/src/scenes/Tiled.js
@@ -4,6 +4,7 @@ class Tiled extends Tableau{
         super.preload();
         this.load.image('tiles', 'assets/tilemaps/tableauTiledTileset.png');
         this.load.tilemapTiledJSON('map', 'assets/tilemaps/tableauTiled.json');
+        this.load.image('platformjump', 'assets/platformjump.png');
         this.load.image('sky-2', 'assets/sky-2.png');
         this.load.image('sky3', 'assets/sky3.png');
         this.load.image('star', 'assets/star.png');
